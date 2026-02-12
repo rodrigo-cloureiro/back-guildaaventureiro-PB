2e313f57d0c5537cc5000169c1348758b1ec48bc
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-12 19:46:15 -0300

    endpoint para retornar os aventureiros filtrados com paginacao

--- src/main/java/br/com/infnet/guildaaventureiro/controller/AventureiroController.java
+++ src/main/java/br/com/infnet/guildaaventureiro/controller/AventureiroController.java
@@ -1,22 +1,50 @@
 package br.com.infnet.guildaaventureiro.controller;
 
 import br.com.infnet.guildaaventureiro.dto.AventureiroCreate;
+import br.com.infnet.guildaaventureiro.dto.AventureiroFiltroRequest;
 import br.com.infnet.guildaaventureiro.dto.AventureiroResponse;
+import br.com.infnet.guildaaventureiro.dto.PagedResponse;
 import br.com.infnet.guildaaventureiro.mapper.AventureiroMapper;
 import br.com.infnet.guildaaventureiro.model.Aventureiro;
 import br.com.infnet.guildaaventureiro.service.AventureiroService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/aventureiro")
+@Validated
 public class AventureiroController {
     private final AventureiroMapper aventureiroMapper;
     private final AventureiroService aventureiroService;
+
+    // =====================
+    // Listar Aventureiros
+    // =====================
+    @GetMapping(value = "")
+    public ResponseEntity<PagedResponse<Aventureiro>> listarAventureiros(
+            @Valid AventureiroFiltroRequest filtro,
+            @RequestHeader(value = "X-Page", required = false, defaultValue = "0")
+            @Min(value = 0, message = "A página não pode ser negativa")
+            int page,
+            @RequestHeader(value = "X-Size", required = false, defaultValue = "25")
+            @Range(min = 1, max = 50, message = "O tamanho deve ser entre 1 e 50")
+            int size
+    ) {
+        PagedResponse<Aventureiro> paginaFiltrada = aventureiroService.listar(filtro, page, size);
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(paginaFiltrada.total()))
+                .header("X-Page", String.valueOf(paginaFiltrada.page()))
+                .header("X-Size", String.valueOf(paginaFiltrada.size()))
+                .header("X-Total-Pages", String.valueOf(paginaFiltrada.totalPages()))
+                .body(paginaFiltrada);
+    }
 
     // =====================
     // Registrar Aventureiro
