ebb2488342773840f437d7e31b45cdbb52ac7534
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-13 19:33:14 -0300

    criando excecao para representar entidade nao encontrada

---
+++ src/main/java/br/com/infnet/guildaaventureiro/exception/EntidadeNaoLocalizadaException.java
@@ -0,0 +1,7 @@
+package br.com.infnet.guildaaventureiro.exception;
+
+public class EntidadeNaoLocalizadaException extends RuntimeException {
+    public EntidadeNaoLocalizadaException(String message) {
+        super(message);
+    }
+}
