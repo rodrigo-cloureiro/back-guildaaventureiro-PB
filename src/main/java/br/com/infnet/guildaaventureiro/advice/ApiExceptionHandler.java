4bae3e10f2ebb85af5f5c39063c24f3e3eda1c97
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-15 17:53:01 -0300

    implementando tratamento das excecoes por requisicao com dados invalidos

--- src/main/java/br/com/infnet/guildaaventureiro/advice/ApiExceptionHandler.java
+++ src/main/java/br/com/infnet/guildaaventureiro/advice/ApiExceptionHandler.java
@@ -1,16 +1,21 @@
 package br.com.infnet.guildaaventureiro.advice;
 
 import br.com.infnet.guildaaventureiro.dto.ErrorResponse;
+import br.com.infnet.guildaaventureiro.enums.Classe;
 import br.com.infnet.guildaaventureiro.exception.EntidadeNaoLocalizadaException;
+import org.jspecify.annotations.Nullable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestControllerAdvice
-// TODO Implementar tratamentos das excecoes por requisicao com dados invalidos
 public class ApiExceptionHandler {
     @ExceptionHandler(EntidadeNaoLocalizadaException.class)
     public ResponseEntity<ErrorResponse> handleEntidadeNaoLocalizadaException(EntidadeNaoLocalizadaException ex) {
@@ -19,5 +24,40 @@ public class ApiExceptionHandler {
                         "Recurso não encontrado",
                         List.of(ex.getMessage())
                 ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleArgumentoInvalidoException(MethodArgumentNotValidException ex) {
+        List<@Nullable String> erros = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        // "Requisição inválida",
+                        // "Dados da requisição inválidos",
+                        "Falha na validação da requisição",
+                        erros
+                ));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handle(HttpMessageNotReadableException ex) {
+        String MESSAGE = "Requisição inválida";
+        ResponseEntity.BodyBuilder response = ResponseEntity.status(HttpStatus.BAD_REQUEST);
+
+        if (ex.getMessage().contains("classe"))
+            return response.body(new ErrorResponse(
+                    MESSAGE,
+                    List.of("Erro no JSON da requisição")
+            ));
+
+        return response.body(new ErrorResponse(
+                MESSAGE,
+                List.of(String.format("Classe inválida. Valores permitidos: %s",
+                        Arrays.toString(Classe.values())
+                ))
+        ));
     }
 }
